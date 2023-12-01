d5431537db257e87e9e21622bd93133c5c7cc346
hjbarangan <hjbarangan@newbrighton.edu.ph>
2023-12-01 14:35:53 +0800

    fix: Fixed validation in add car

--- src/validation/car.validation.js
+++ src/validation/car.validation.js
@@ -3,40 +3,27 @@ const validateCar = async (car) => {
 
   // Validate brand
   if (!brand) {
-    throw new Error("Missing required field: 'brand'");
+    throw new Error("Missing required field: brand");
   }
 
   // Validate model
   if (!model) {
-    throw new Error("Missing required field: 'model'");
+    throw new Error("Missing required field: model");
   }
 
   // Validate year
-  if (!year || typeof year !== "number") {
-    throw new Error("Year must be a valid number");
+  if (!year) {
+    throw new Error("Missing required field: year");
   }
 
   // Validate serial_no
   if (!serial_no) {
-    throw new Error("Missing required field: 'serial_no'");
-  }
-  if (serial_no.length < 5) {
-    throw new Error("Serial number must be at least 5 characters long");
+    throw new Error("Missing required field: serial no");
   }
 
   // Validate color
   if (!color) {
-    throw new Error("Missing required field: 'color'");
-  }
-
-  // Validate car_price
-  if (!car_price || typeof car_price !== "number") {
-    throw new Error("Car price must be a valid number");
-  }
-
-  // Validate brand_new
-  if (!brand_new || typeof brand_new !== 'boolean') {
-    throw new Error("Brand new must be a valid boolean value (true or false)");
+    throw new Error("Missing required field: color");
   }
 };
 
